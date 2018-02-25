6d49d071ac46bf5e6774bf84d710815f32f5baae
Jacques Nel <jmnel92@gmail.com>
2018-02-25 02:31:02 -0500

    First commit.

---
+++ src/tutorial0/Main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char *argv[]) {
+
+  cout << "Hello world!" << endl;
+  cout << "I am the product of a cmake project!" << endl;
+
+  return EXIT_SUCCESS;
+}
